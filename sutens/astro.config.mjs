e484e1d0004d1e1678aba309e1562024b269086f
Luiggyblanco24 <24luiggy@gmail.com>
2025-10-07 13:56:46 -0500

    config vercel

--- sutens/astro.config.mjs
+++ sutens/astro.config.mjs
@@ -1,8 +1,7 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
-import vercel from "@astrojs/vercel"; // ✅ sin /server
+import vercel from "@astrojs/vercel"; // ✅ Import correcto
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +9,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   integrations: [icon()],
-  output: "server",
-  adapter: vercel({ mode: "server" }), // ✅ modo explícito
+  output: "server", // o "static" si no usas SSR
+  adapter: vercel({ mode: "server" }), // ✅ Modo explícito
 });
 
